2bcff00058e2aa13ab93b1fbb5a6acd1d183fdd4
caesar <caesar.lee@mirrormedia.mg>
2022-09-03 14:44:16 +0800

    refactor(mm-search): fetch header datats

--- packages/mirror-media-search/pages/index.js
+++ packages/mirror-media-search/pages/index.js
@@ -1,4 +1,12 @@
+import axios from 'axios'
 import Header from '../components/shared/mirror-media-header-old'
+import {
+  URL_STATIC_COMBO_SECTIONS,
+  API_TIMEOUT,
+  API_PROTOCOL,
+  API_HOST,
+  API_PORT,
+} from '../config'
 
 export default function Home() {
   return (
@@ -7,4 +15,37 @@ export default function Home() {
       <div>Hello Mirror Media Search</div>
     </>
   )
+}
+
+export async function getServerSideProps() {
+  try {
+    const props = {}
+
+    const responses = await Promise.allSettled([
+      axios({
+        method: 'get',
+        url: URL_STATIC_COMBO_SECTIONS,
+        timeout: API_TIMEOUT,
+      }),
+      axios({
+        method: 'get',
+        url: `${API_PROTOCOL}://${API_HOST}:${API_PORT}/combo?endpoint=topics`,
+        timeout: API_TIMEOUT,
+      }),
+    ])
+
+    props.sectionsData = responses[0].value.data._items
+    props.topicsData = responses[1].value.data._endpoints.topics._items
+
+    console.log(
+      JSON.stringify({
+        severity: 'DEBUG',
+        message: `Successfully fetch sections and topics from ${URL_STATIC_COMBO_SECTIONS} and ${API_PROTOCOL}://${API_HOST}:${API_PORT}/combo?endpoint=topics`,
+      })
+    )
+    return { props }
+  } catch (error) {
+    console.log(JSON.stringify({ severity: 'ERROR', message: error.stack }))
+    return { props: { sectionsData: [], topicsData: [] } }
+  }
 }
