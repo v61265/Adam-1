ee5a0e916ed2fc70efccab266da29e49b3bda506
caesar <caesar.lee@mirrormedia.mg>
2022-09-26 17:59:22 +0800

    refactor(mm-search): add date strongly bias

--- packages/mirror-media-search/utils/api/programmable-search.js
+++ packages/mirror-media-search/utils/api/programmable-search.js
@@ -46,7 +46,7 @@ export async function getSearchResult(query) {
       q: params.query,
       start: params.start,
       num: params.take,
-      sort: 'date',
+      sort: 'date:d:s',
     }
 
     const prefix = 'PROGRAMABLE_SEARCH'
