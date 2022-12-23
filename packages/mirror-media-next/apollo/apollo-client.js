f32dd890bc2a8fc0ed24b450ea50588abef2499d
Dian-Yang Fu <85677992+dyfu95@users.noreply.github.com>
2022-12-23 14:20:33 +0800

    feat(mm-next): add Dep `apollo-client` and set related setting (#75)

---
+++ packages/mirror-media-next/apollo/apollo-client.js
@@ -0,0 +1,8 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { API_HOST } from '../config'
+const client = new ApolloClient({
+  uri: `https://${API_HOST}/api/graphql`,
+  cache: new InMemoryCache(),
+})
+
+export default client
