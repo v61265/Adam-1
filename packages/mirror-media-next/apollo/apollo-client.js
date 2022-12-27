3a80cca4cb8366f24acc398bcbdeaa0624e7a87b
Dian-Yang Fu <85677992+dyfu95@users.noreply.github.com>
2022-12-28 00:54:51 +0800

    refactor(mm-next): add `trace` in error log (#88)

--- packages/mirror-media-next/apollo/apollo-client.js
+++ packages/mirror-media-next/apollo/apollo-client.js
@@ -1,67 +1,61 @@
 // We use apollo/link to print log when error occur at executing GraphQL operations,
 // see docs to get more information:
-// https://www.apollographql.com/docs/react/api/link/introduction
 // https://www.apollographql.com/docs/react/data/error-handling/#advanced-error-handling-with-apollo-link
 
-import {
-  ApolloClient,
-  ApolloLink,
-  HttpLink,
-  InMemoryCache,
-  from,
-} from '@apollo/client'
+import { ApolloClient, HttpLink, InMemoryCache, from } from '@apollo/client'
 import { onError } from '@apollo/client/link/error'
+
 import { API_HOST, GCP_PROJECT_ID } from '../config'
+
 const httpLink = new HttpLink({ uri: `https://${API_HOST}/api/graphql` })
 
-const getTraceHeaderMiddleware = new ApolloLink((operation, forward) => {
-  const globalLogFields = {}
-  const headers = operation.getContext()?.headers
-  const traceHeader = headers?.['X-Cloud-Trace-Context']
-  if (headers && traceHeader) {
-    const [trace] = traceHeader.split('/')
-    globalLogFields[
-      'logging.googleapis.com/trace'
-    ] = `projects/${GCP_PROJECT_ID}/traces/${trace}`
-    operation.setContext({ globalLogFields })
-  }
+const errorLink = onError(
+  ({ graphQLErrors, networkError, operation, forward }) => {
+    const { response } = operation.getContext()
 
-  return forward(operation)
-})
+    const { headers } = response
+    const traceHeader = headers.get('X-Cloud-Trace-Context')
 
-const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
-  const globalLogFields = operation.getContext()?.globalLogFields || {}
+    let globalLogFields = {}
+    if (headers && traceHeader) {
+      const [trace] = traceHeader.split('/')
+      globalLogFields[
+        'logging.googleapis.com/trace'
+      ] = `projects/${GCP_PROJECT_ID}/traces/${trace}`
+    }
 
-  let debugPayload = {
-    graphQLOperationName: operation.operationName,
-    graphQLErrors: [],
-    networkError: '',
-  }
+    let debugPayload = {
+      graphQLOperationName: operation.operationName,
+      graphQLErrors: [],
+      networkError: '',
+    }
 
-  if (graphQLErrors)
-    graphQLErrors.forEach(({ message, locations, path }) => {
-      debugPayload.graphQLErrors.push({
-        Message: message,
-        Location: JSON.stringify(locations),
-        Path: `${path}`,
+    if (graphQLErrors)
+      graphQLErrors.forEach(({ message, locations, path }) => {
+        debugPayload.graphQLErrors.push({
+          Message: message,
+          Location: JSON.stringify(locations),
+          Path: `${path}`,
+        })
       })
-    })
 
-  if (networkError) {
-    debugPayload.networkError = `${networkError}`
+    if (networkError) {
+      debugPayload.networkError = `${networkError}`
+    }
+    console.error(
+      JSON.stringify({
+        severity: 'ERROR',
+        message: `[Error] Fetch graphQL failed`,
+        debugPayload,
+        ...globalLogFields,
+      })
+    )
+    return forward(operation)
   }
-  console.error(
-    JSON.stringify({
-      severity: 'ERROR',
-      message: `[Error] Fetch graphQL failed`,
-      debugPayload,
-      ...globalLogFields,
-    })
-  )
-})
+)
 
 const client = new ApolloClient({
-  link: from([getTraceHeaderMiddleware, errorLink, httpLink]),
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
 })
 
