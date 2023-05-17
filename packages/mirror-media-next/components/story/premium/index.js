84c662df7600fd18655d052b5fd7a165e5fc5fe0
DianYangFu <dianyangfu@gmail.com>
2023-05-17 18:10:31 +0800

    feat(mm-next): show header in story premium layout

--- packages/mirror-media-next/components/story/premium/index.js
+++ packages/mirror-media-next/components/story/premium/index.js
@@ -1,9 +1,24 @@
+import { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import ShareHeader from '../../shared/share-header'
 import DraftRenderBlock from '../shared/draft-renderer-block'
 import ArticleBrief from '../shared/brief'
+import { fetchHeaderDataInPremiumPageLayout } from '../../../utils/api'
+import errors from '@twreporter/errors'
 /**
  * @typedef {import('../../../apollo/fragments/post').Post} PostData
  */
+
+const HeaderPlaceHolder = styled.header`
+  background-color: transparent;
+  height: 101px;
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  ${({ theme }) => theme.breakpoint.md} {
+    height: 115px;
+  }
+`
 
 const Main = styled.main`
   width: 100%;
@@ -39,24 +54,74 @@ const ContentWrapper = styled.section`
  * @returns {JSX.Element}
  */
 export default function StoryPremiumStyle({ postData }) {
+  const [headerData, setHeaderData] = useState({
+    sectionsData: [],
+  })
+  const [isHeaderDataLoaded, setIsHeaderDataLoaded] = useState(false)
+
+  useEffect(() => {
+    let ignore = false
+    fetchHeaderDataInPremiumPageLayout()
+      .then((res) => {
+        if (!ignore && !isHeaderDataLoaded) {
+          const { sectionsData } = res
+          setHeaderData({ sectionsData })
+          setIsHeaderDataLoaded(true)
+        }
+      })
+      .catch((error) => {
+        if (!ignore && !isHeaderDataLoaded) {
+          console.log(
+            errors.helpers.printAll(
+              error,
+              {
+                withStack: true,
+                withPayload: true,
+              },
+              0,
+              0
+            )
+          )
+          setIsHeaderDataLoaded(true)
+        }
+      })
+
+    return () => {
+      ignore = true
+    }
+  }, [isHeaderDataLoaded])
+
   const { content, brief } = postData
   return (
-    <Main>
-      <article>
-        <ContentWrapper>
-          <section className="content">
-            <ArticleBrief
-              sectionSlug="member"
-              brief={brief}
-              contentLayout="premium"
-            ></ArticleBrief>
-            <DraftRenderBlock
-              contentLayout="premium"
-              rawContentBlock={content}
-            ></DraftRenderBlock>
-          </section>
-        </ContentWrapper>
-      </article>
-    </Main>
+    <>
+      {isHeaderDataLoaded ? (
+        <ShareHeader
+          pageLayoutType="premium"
+          headerData={{
+            sectionsData: headerData.sectionsData,
+          }}
+        ></ShareHeader>
+      ) : (
+        <HeaderPlaceHolder />
+      )}
+
+      <Main>
+        <article>
+          <ContentWrapper>
+            <section className="content">
+              <ArticleBrief
+                sectionSlug="member"
+                brief={brief}
+                contentLayout="premium"
+              ></ArticleBrief>
+              <DraftRenderBlock
+                contentLayout="premium"
+                rawContentBlock={content}
+              ></DraftRenderBlock>
+            </section>
+          </ContentWrapper>
+        </article>
+      </Main>
+    </>
   )
 }
