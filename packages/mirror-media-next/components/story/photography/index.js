f3e133d76188683392978662c316373d3d26e512
bibibobobi <smilingcat33@gmail.com>
2023-05-19 17:28:17 +0800

    styles(mm-next/photography): add heroImage and credits styles

--- packages/mirror-media-next/components/story/photography/index.js
+++ packages/mirror-media-next/components/story/photography/index.js
@@ -1,12 +1,80 @@
+// @ts-nocheck
 import styled from 'styled-components'
 import DraftRenderBlock from '../shared/draft-renderer-block'
-// import { MirrorMedia } from '@mirrormedia/lilith-draft-renderer'
-import Credits from '../wide/credits'
+import Credits from './credits'
 
 const Main = styled.main`
+  height: auto;
   margin: auto;
+  padding-bottom: 40px;
   width: 100%;
-  background-color: darkgray;
+  background-color: #333333;
+
+  .content {
+    margin-top: 40px;
+    // Only render contents besides the images and captions via draft renderer.
+    figure {
+      display: none;
+    }
+  }
+
+  p {
+    color: white;
+  }
+`
+
+const HeroImage = styled.div`
+  background-image: linear-gradient(
+      to bottom,
+      rgba(255, 255, 255, 0),
+      rgba(0, 0, 0, 0.4)
+    ),
+    url(${(props) => props.imageUrl});
+
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`
+
+const TitleBox = styled.div`
+  color: white;
+  width: 80%;
+  margin: 0 auto;
+  text-align: center;
+  margin-bottom: 32px;
+  font-family: var(--inter-font);
+  font-style: normal;
+  font-weight: 400;
+  text-shadow: 0.9px 0px 0.5px rgba(0, 0, 0, 0.8);
+
+  .title {
+    font-size: 40px;
+    line-height: 48px;
+    color: #ffffff;
+  }
+
+  .hero-caption {
+    font-size: 16px;
+    line-height: 20px;
+    color: #d1d1d1;
+    margin: 32px 0;
+  }
+
+  .brief {
+    font-size: 16px;
+    line-height: 20px;
+    color: #ffffff;
+  }
+`
+
+const Container = styled.div`
+  width: 80%;
+  margin: auto;
 `
 /**
  * @typedef {import('../../../apollo/fragments/post').Post} PostData
@@ -21,9 +89,9 @@ const Main = styled.main`
 export default function StoryPhotographyStyle({ postData }) {
   console.log(postData)
   const {
-    // title = '',
-    // heroImage = null,
-    // heroCaption = '',
+    title = '',
+    heroImage = null,
+    heroCaption = '',
     // updatedAt = '',
     // publishedDate = '',
     writers = [],
@@ -51,17 +119,38 @@ export default function StoryPhotographyStyle({ postData }) {
     { extend_byline: extend_byline },
   ]
 
+  // Get images array from content.entityMap
+  const imageArray = Object.values(content.entityMap).filter(
+    (item) => item.type === 'image'
+  )
+
+  console.log(imageArray)
+
   return (
     <Main>
-      <div>這是photography版型</div>
-      <section className="content">
-        <DraftRenderBlock rawContentBlock={brief} contentLayout="photography" />
-        <DraftRenderBlock
-          rawContentBlock={content}
-          contentLayout="photography"
-        />
-      </section>
-      <Credits credits={credits}></Credits>
+      <HeroImage
+        imageUrl={
+          heroImage?.resized?.original ||
+          heroImage?.resized?.w2400 ||
+          heroImage?.resized?.w1600 ||
+          ''
+        }
+      >
+        <TitleBox>
+          <h1 className="title">{title}</h1>
+          <p className="hero-caption">{heroCaption}</p>
+          <p className="brief">{brief.blocks[0].text}</p>
+        </TitleBox>
+      </HeroImage>
+      <Container>
+        <section className="content">
+          <DraftRenderBlock
+            rawContentBlock={content}
+            contentLayout="photography"
+          />
+        </section>
+        <Credits credits={credits}></Credits>
+      </Container>
     </Main>
   )
 }
