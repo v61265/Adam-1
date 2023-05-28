e15608d48f1a6a0cfcbc27e86ae625cf8a369fd6
caesar <caesar.lee@mirrormedia.mg>
2023-05-28 21:35:02 +0800

    feat(mm-next): add Avivid ad to whole site

--- packages/mirror-media-next/components/whole-site-script.js
+++ packages/mirror-media-next/components/whole-site-script.js
@@ -1,9 +1,11 @@
 import GPTScript from './ads/gpt/gpt-script'
+import AvividScript from './ads/avivid/avivid-script'
 
 export default function WholeSiteScript() {
   return (
     <>
       <GPTScript />
+      <AvividScript />
     </>
   )
 }
