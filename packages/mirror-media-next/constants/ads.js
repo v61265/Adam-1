8929d81257cd919d02886bc33e41933bed0aee60
caesar <caesar.lee@mirrormedia.mg>
2023-05-28 15:29:30 +0800

    feat(mm-next): add MicroAd component

--- packages/mirror-media-next/constants/ads.js
+++ packages/mirror-media-next/constants/ads.js
@@ -1,5 +1,41 @@
 import { SECTION_IDS } from '.'
 
+const MICRO_AD_UNITS = {
+  HOME: {
+    PC: [
+      { name: 'NA1_PC_HP', id: '4273362' },
+      { name: 'NA2_PC_HP', id: '4273366' },
+      { name: 'NA3_PC_HP', id: '4273370' },
+    ],
+    MB: [
+      { name: 'NA1_MB_HP', id: '4273363' },
+      { name: 'NA2_MB_HP', id: '4273367' },
+      { name: 'NA3_MB_HP', id: '4273371' },
+    ],
+  },
+  LISTING: {
+    RWD: [
+      { name: 'NA1_RWD_SP', id: '4273364' },
+      { name: 'NA2_RWD_SP', id: '4273368' },
+      { name: 'NA3_RWD_SP', id: '4273372' },
+    ],
+  },
+  STORY: {
+    PC: [
+      { name: 'NA1_PC_AP', id: '4276377' },
+      { name: 'NA2_PC_AP', id: '4276378' },
+      { name: 'NA3_PC_AP', id: '4276379' },
+      { name: 'NA4_PC_AP', id: '4297298' },
+    ],
+    MB: [
+      { name: 'NA1_MB_AP', id: '4273365' },
+      { name: 'NA2_MB_AP', id: '4273369' },
+      { name: 'NA3_MB_AP', id: '4273373' },
+      { name: 'NA4_MB_AP', id: '4297297' },
+    ],
+  },
+}
+
 const {
   member,
   news,
@@ -1510,4 +1546,4 @@ const GPT_UNITS = {
 
 const GPT_AD_NETWORK = '40175602'
 
-export { GPT_UNITS, GPT_AD_NETWORK }
+export { MICRO_AD_UNITS, GPT_UNITS, GPT_AD_NETWORK }
