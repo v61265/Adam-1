cbbf65cb7b62f16550599dd563873e8faf53069a
Xuan <as840922qq@gmail.com>
2023-05-30 12:46:35 +0800

    feat(external): add `external` components

---
+++ packages/mirror-media-next/components/external/external-article-info.js
@@ -0,0 +1,133 @@
+import styled from 'styled-components'
+import Link from 'next/link'
+import Image from 'next/image'
+import ButtonCopyLink from '../../components/story/shared/button-copy-link'
+import DonateLink from '../../components/story/shared/donate-link'
+import ButtonSocialNetworkShare from '../../components/story/shared/button-social-network-share'
+import { getCreditsHtml } from '../../utils/external'
+
+/**
+ * @typedef {import('~/type/theme').Theme} Theme
+ */
+
+const Date = styled.div`
+  width: fit-content;
+  height: auto;
+  font-size: 14px;
+  line-height: 1;
+  color: #a1a1a1;
+  margin-bottom: 8px;
+  ${({ theme }) => theme.breakpoint.md} {
+    display: none;
+  }
+`
+
+const SocialMedia = styled.div`
+  display: flex;
+  gap: 10px;
+  padding: 0;
+  position: relative;
+  margin-bottom: 20px;
+
+  ${({ theme }) => theme.breakpoint.md} {
+    padding: 0 40px;
+    margin-bottom: 0px;
+    &::before,
+    &::after {
+      position: absolute;
+      content: '';
+      background-color: #a1a1a1;
+      width: 1px;
+      height: 16px;
+      transform: translateY(-50%);
+      top: 50%;
+    }
+    &::before {
+      left: 20px;
+    }
+    &::after {
+      right: 20px;
+    }
+  }
+`
+
+const SocialMediaAndDonateLink = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: 20px;
+
+  ${({ theme }) => theme.breakpoint.md} {
+    margin-top: 40px;
+    flex-direction: row;
+    align-items: center;
+  }
+  .link-to-index {
+    display: none;
+    ${({ theme }) => theme.breakpoint.md} {
+      display: block;
+    }
+  }
+`
+
+const ArticleInfoContainer = styled.div`
+  border-left: 2px ${({ theme }) => theme.color.brandColor.darkBlue} solid;
+  padding-left: 24px;
+  margin: 32px 0;
+  ${({ theme }) => theme.breakpoint.md} {
+    border: none;
+    padding-left: 0px;
+    margin: 0 0 24px;
+  }
+`
+
+const ExternalCredit = styled.div`
+  margin: 25px 0px;
+  text-align: left;
+  color: rgb(52, 73, 94);
+  line-height: 1.5;
+
+  a {
+    color: #0b4fa2;
+  }
+`
+
+/**
+ * @param {Object} props
+ * @param {string} props.updatedDate
+ * @param {string} props.publishedDate
+ * @param {string} props.credits
+ * @returns {JSX.Element}
+ */
+export default function ArticleInfo({ updatedDate, publishedDate, credits }) {
+  const creditJsx = credits.length > 0 && (
+    <ExternalCredit>文｜{getCreditsHtml(credits)} </ExternalCredit>
+  )
+
+  return (
+    <ArticleInfoContainer>
+      <Date>發布時間：{publishedDate} 臺北時間</Date>
+      <Date>更新時間：{updatedDate} 臺北時間</Date>
+
+      {creditJsx}
+
+      <SocialMediaAndDonateLink>
+        <Link className="link-to-index" href="/">
+          <Image
+            width={35}
+            height={35}
+            alt="go-to-index-page"
+            src="/images/logo-circle@2x.png"
+          ></Image>
+        </Link>
+        <SocialMedia>
+          <ButtonSocialNetworkShare type="facebook" />
+          <ButtonSocialNetworkShare type="line" />
+          <ButtonCopyLink />
+        </SocialMedia>
+        <DonateLink />
+      </SocialMediaAndDonateLink>
+    </ArticleInfoContainer>
+  )
+}
---
+++ packages/mirror-media-next/components/external/external-hero-image.js
@@ -0,0 +1,54 @@
+import CustomImage from '@readr-media/react-image'
+import styled from 'styled-components'
+
+const Wrapper = styled.figure`
+  margin: 20px 0 0;
+  ${({ theme }) => theme.breakpoint.md} {
+    margin: 0 0 34px;
+  }
+`
+
+const HeroImage = styled.figure`
+  position: relative;
+  width: 100%;
+  height: 58.75vw;
+  .readr-media-react-image {
+    object-position: center center;
+  }
+
+  ${({ theme }) => theme.breakpoint.md} {
+    width: 640px;
+    height: 428px;
+  }
+`
+
+/**
+ * @typedef {import('../../apollo/fragments/photo').Resized} Resized
+ */
+/**
+ * @param {Object} props
+ * @param {Resized} props.images
+ * @param {string} props.title
+ * @param {string} [props.className]
+ * @returns
+ */
+export default function ExternalHeroImage({
+  images = {},
+  title = '',
+  className = '',
+}) {
+  return (
+    <Wrapper className={className}>
+      <HeroImage>
+        <CustomImage
+          images={images}
+          loadingImage={'/images/loading@4x.gif'}
+          defaultImage={'/images/default-og-img.png'}
+          alt={title}
+          objectFit={'cover'}
+          priority={true}
+        />
+      </HeroImage>
+    </Wrapper>
+  )
+}
