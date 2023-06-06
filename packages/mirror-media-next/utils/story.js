7afc4f6b1b7ef9c0ed0ea73f9b94b1f7da2ab5ea
Xuan <as840922qq@gmail.com>
2023-06-06 15:24:30 +0800

    feat(story): add `handleStoryPageRedirect` utils

---
+++ packages/mirror-media-next/utils/story.js
@@ -0,0 +1,42 @@
+/**
+ * @typedef {Object} Redirect
+ * @property {{ destination: string,permanent: boolean }} redirect
+ */
+/**
+ *
+ * @param {string} redirect - post's redirect URL
+ * @returns {Redirect}
+ */
+
+const handleStoryPageRedirect = (redirect) => {
+  if (redirect && redirect.trim()) {
+    const redirectHref = redirect.trim()
+    if (
+      redirectHref.startsWith('https://') ||
+      redirectHref.startsWith('http://')
+    ) {
+      return {
+        redirect: {
+          destination: `${redirectHref} `,
+          permanent: false,
+        },
+      }
+    } else if (redirectHref.startsWith('www.')) {
+      return {
+        redirect: {
+          destination: `https://${redirectHref}`,
+          permanent: false,
+        },
+      }
+    } else {
+      return {
+        redirect: {
+          destination: `/story/${redirectHref} `,
+          permanent: false,
+        },
+      }
+    }
+  }
+}
+
+export { handleStoryPageRedirect }
