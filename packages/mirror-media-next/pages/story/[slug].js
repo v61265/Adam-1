f201b64ce25a1720a0c4481b83927cdfe5f7ff97
DianYangFu <dianyangfu@gmail.com>
2023-06-08 13:30:51 +0800

    feat(mm-next): change condition of decide which story layout should show

--- packages/mirror-media-next/pages/story/[slug].js
+++ packages/mirror-media-next/pages/story/[slug].js
@@ -1,5 +1,5 @@
 //TODO: add component to add html head dynamically, not jus write head in every pag
-import { useState, useEffect } from 'react'
+
 import client from '../../apollo/apollo-client'
 import errors from '@twreporter/errors'
 import styled from 'styled-components'
@@ -28,7 +28,6 @@ import Skeleton from '../../public/images/skeleton.png'
  * @typedef {import('../../components/story/normal').PostData} PostData
  */
 
-//Todo: adjust height, make it not to scroll when loading
 const Loading = styled.div`
   width: 100%;
   height: 100%;
@@ -39,47 +38,6 @@ const Loading = styled.div`
     margin: 0 auto;
   }
 `
-/**
- *
- * @param {'article'| 'wide' | 'projects' | 'photography' | 'script' | 'campaign' | 'readr'} articleType
- * @param {boolean} isMemberArticle
- * @param { 'yearly' | 'monthly' | 'basic' | undefined} [memberType]
- * @return {'style-normal' | 'style-wide' | 'style-photography' | 'style-premium'}
- */
-const getStoryLayout = (
-  articleType,
-  isMemberArticle = false,
-  memberType = undefined
-) => {
-  switch (articleType) {
-    case 'wide':
-      return 'style-wide'
-
-    case 'photography':
-      return 'style-photography'
-
-    case 'article':
-      if (
-        isMemberArticle ||
-        memberType === 'monthly' ||
-        memberType === 'yearly'
-      ) {
-        return 'style-premium'
-      }
-      return 'style-normal'
-
-    default:
-      return 'style-normal'
-  }
-}
-
-const mockMemberSystem = () => {
-  return new Promise((resolve) =>
-    setTimeout(() => {
-      resolve('basic')
-    }, 1000)
-  )
-}
 
 /**
  *
@@ -96,42 +54,19 @@ export default function Story({ postData }) {
     categories = [],
   } = postData
 
-  const [storyLayout, setStoryLayout] = useState(null)
-  // add pop in script for example, need to place to the right postition (story type)
-
   const renderStoryLayout = () => {
-    switch (storyLayout) {
-      case 'style-normal':
-        return <StoryNormalStyle postData={postData} />
-      case 'style-wide':
-        return <StoryWideStyle postData={postData} />
-      case 'style-photography':
-        return <StoryPhotographyStyle postData={postData} />
-      case 'style-premium':
-        return <StoryPremiumStyle postData={postData} />
-      default:
-        return <StoryNormalStyle postData={postData} />
+    if (style === 'wide') {
+      return <StoryWideStyle postData={postData} />
+    } else if (style === 'photography') {
+      return <StoryPhotographyStyle postData={postData} />
+    } else if (style === 'article' && isMember === true) {
+      return <StoryPremiumStyle postData={postData} />
     }
+    return <StoryNormalStyle postData={postData} />
   }
-  const jsx = renderStoryLayout()
+  const storyLayout = renderStoryLayout()
 
   //mock for process of changing article type
-  useEffect(() => {
-    async function getMemberType() {
-      const memberType = await mockMemberSystem()
-      return memberType
-    }
-
-    getMemberType()
-      .then((res) => {
-        const storyLayout = getStoryLayout(style, isMember, res)
-        setStoryLayout(storyLayout)
-      })
-      .catch(() => {
-        const storyLayout = getStoryLayout(style, isMember, undefined)
-        setStoryLayout(storyLayout)
-      })
-  }, [style, isMember])
 
   return (
     <Layout
@@ -153,11 +88,7 @@ export default function Story({ postData }) {
             <Image src={Skeleton} alt="loading..."></Image>
           </Loading>
         )}
-
-        <div style={{ display: `${storyLayout ? 'block' : 'none'}` }}>
-          {jsx}
-        </div>
-
+        {storyLayout}
         <WineWarning categories={categories} />
         <AdultOnlyWarning isAdult={isAdult} />
       </>
