db3298d0a98a2762c5c7de5fa8355029285944a1
caesarWHLee <104348940+caesarWHLee@users.noreply.github.com>
2023-06-08 21:35:20 +0800

    Update run.sh

--- packages/mirror-media-next/run.sh
+++ packages/mirror-media-next/run.sh
@@ -4,10 +4,10 @@
 if [ "$PROXY_AMP" = "true" ]
 then
   # run next.js together with proxy server
-  PORT=$PROXIED_SERVER_PORT node server.js &
+  PORT=$PROXIED_SERVER_PORT yarn start &
   yarn run start-amp-proxy-server &
 else
-  node server.js &
+  yarn start &
 fi
 
 # Exit immediately when one of the background processes terminate.
