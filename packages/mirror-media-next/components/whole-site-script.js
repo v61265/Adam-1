0153b0e68805e2c9d23af878e7ab298868d2bd6d
Xuan <as840922qq@gmail.com>
2023-06-29 11:40:04 +0800

    feat(avivid): add `shouldShowAd` in Avivid ad

--- packages/mirror-media-next/components/whole-site-script.js
+++ packages/mirror-media-next/components/whole-site-script.js
@@ -1,11 +1,13 @@
 import GPTScript from './ads/gpt/gpt-script'
 import AvividScript from './ads/avivid/avivid-script'
+import { useDisplayAd } from '../hooks/useDisplayAd'
 
 export default function WholeSiteScript() {
+  const shouldShowAd = useDisplayAd()
   return (
     <>
       <GPTScript />
-      <AvividScript />
+      {shouldShowAd && <AvividScript />}
     </>
   )
 }
