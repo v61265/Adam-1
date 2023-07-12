c5462f26d4f18675846db304f5bf39e4d78a06c5
bibibobobi <smilingcat33@gmail.com>
2023-07-12 10:40:25 +0800

    fix(mm-next/amp-related): add PopIn Ad for AMP page

---
+++ packages/mirror-media-next/components/amp/amp-ads/amp-popin-ad.js
@@ -0,0 +1,29 @@
+// @ts-nocheck
+import styled from 'styled-components'
+
+// The AMP PopIn ads only appear on mobile screens. To prevent blank space, the component should not be displayed when the screen width is greater than 'md'.
+const PopInWrapper = styled.div`
+  ${({ theme }) => theme.breakpoint.md} {
+    display: none;
+  }
+`
+/**
+ * Renders a Popin component.
+ *
+ * @return {JSX.Element} The rendered Popin component.
+ */
+
+export default function AmpPopIn() {
+  return (
+    <PopInWrapper>
+      <amp-ad
+        type="popin"
+        width={300}
+        height={568}
+        layout="responsive"
+        heights="(min-width:1907px) 39%, (min-width:1200px) 46%, (min-width:780px) 64%, (min-width:480px) 98%, (min-width:460px) 167%, 196%"
+        data-mediaid="mirrormedia_AMP"
+      />
+    </PopInWrapper>
+  )
+}
