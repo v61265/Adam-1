05a5dc3bd00f056fa3ee609e770ec6c54d44b549
bibibobobi <smilingcat33@gmail.com>
2023-07-25 13:48:22 +0800

    fix(mm-next/papermag): add page papermag and fetch header

---
+++ packages/mirror-media-next/pages/papermag/2.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components'
+import errors from '@twreporter/errors'
+import { GCP_PROJECT_ID } from '../../config/index.mjs'
+import { fetchHeaderDataInDefaultPageLayout } from '../../utils/api'
+import { setPageCache } from '../../utils/cache-setting'
+import Layout from '../../components/shared/layout'
+
+const Page = styled.div`
+  min-height: 65vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+/**
+ * @param {Object} props
+ * @param {Object[] } props.sectionsData
+ * @param {Object[]} props.topicsData
+ * @return {JSX.Element}
+ */
+function TwoYearSubscription({ sectionsData = [], topicsData = [] }) {
+  return (
+    <Layout
+      head={{ title: `訂閱二年方案` }}
+      header={{
+        type: 'default',
+        data: { sectionsData: sectionsData, topicsData },
+      }}
+      footer={{ type: 'default' }}
+    >
+      <Page>訂閱二年</Page>
+    </Layout>
+  )
+}
+
+export default TwoYearSubscription
+
+/**
+ * @type {import('next').GetServerSideProps}
+ */
+export async function getServerSideProps({ req, res }) {
+  setPageCache(res, { cachePolicy: 'no-store' }, req.url)
+
+  const traceHeader = req.headers?.['x-cloud-trace-context']
+  let globalLogFields = {}
+  if (traceHeader && !Array.isArray(traceHeader)) {
+    const [trace] = traceHeader.split('/')
+    globalLogFields[
+      'logging.googleapis.com/trace'
+    ] = `projects/${GCP_PROJECT_ID}/traces/${trace}`
+  }
+
+  // Fetch header data
+  let sectionsData = []
+  let topicsData = []
+
+  try {
+    const headerData = await fetchHeaderDataInDefaultPageLayout()
+    if (Array.isArray(headerData.sectionsData)) {
+      sectionsData = headerData.sectionsData
+    }
+    if (Array.isArray(headerData.topicsData)) {
+      topicsData = headerData.topicsData
+    }
+  } catch (err) {
+    const annotatingAxiosError = errors.helpers.annotateAxiosError(err)
+    console.error(
+      JSON.stringify({
+        severity: 'ERROR',
+        message: errors.helpers.printAll(annotatingAxiosError, {
+          withStack: true,
+          withPayload: true,
+        }),
+        ...globalLogFields,
+      })
+    )
+  }
+
+  return {
+    props: {
+      sectionsData,
+      topicsData,
+    },
+  }
+}
---
+++ packages/mirror-media-next/pages/papermag/index.js
@@ -0,0 +1,86 @@
+import styled from 'styled-components'
+import errors from '@twreporter/errors'
+import { GCP_PROJECT_ID } from '../../config/index.mjs'
+import { fetchHeaderDataInDefaultPageLayout } from '../../utils/api'
+import { setPageCache } from '../../utils/cache-setting'
+import Layout from '../../components/shared/layout'
+
+const Page = styled.div`
+  min-height: 65vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+/**
+ * @param {Object} props
+ * @param {Object[] } props.sectionsData
+ * @param {Object[]} props.topicsData
+ * @return {JSX.Element}
+ */
+function PaperMag({ sectionsData = [], topicsData = [] }) {
+  return (
+    <Layout
+      head={{ title: `訂閱紙本雜誌` }}
+      header={{
+        type: 'default',
+        data: { sectionsData: sectionsData, topicsData },
+      }}
+      footer={{ type: 'default' }}
+    >
+      <Page>
+        <p>紙本雜誌訂閱 方案選擇頁</p>
+      </Page>
+    </Layout>
+  )
+}
+
+export default PaperMag
+
+/**
+ * @type {import('next').GetServerSideProps}
+ */
+export async function getServerSideProps({ req, res }) {
+  setPageCache(res, { cachePolicy: 'no-store' }, req.url)
+
+  const traceHeader = req.headers?.['x-cloud-trace-context']
+  let globalLogFields = {}
+  if (traceHeader && !Array.isArray(traceHeader)) {
+    const [trace] = traceHeader.split('/')
+    globalLogFields[
+      'logging.googleapis.com/trace'
+    ] = `projects/${GCP_PROJECT_ID}/traces/${trace}`
+  }
+
+  // Fetch header data
+  let sectionsData = []
+  let topicsData = []
+
+  try {
+    const headerData = await fetchHeaderDataInDefaultPageLayout()
+    if (Array.isArray(headerData.sectionsData)) {
+      sectionsData = headerData.sectionsData
+    }
+    if (Array.isArray(headerData.topicsData)) {
+      topicsData = headerData.topicsData
+    }
+  } catch (err) {
+    const annotatingAxiosError = errors.helpers.annotateAxiosError(err)
+    console.error(
+      JSON.stringify({
+        severity: 'ERROR',
+        message: errors.helpers.printAll(annotatingAxiosError, {
+          withStack: true,
+          withPayload: true,
+        }),
+        ...globalLogFields,
+      })
+    )
+  }
+
+  return {
+    props: {
+      sectionsData,
+      topicsData,
+    },
+  }
+}
