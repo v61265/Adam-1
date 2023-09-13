29c88561dd0326ed896806643c7be1eb9d19ef3f
YiHsuan <v61265@gmail.com>
2023-09-13 13:48:05 +0800

    feat: add /subscribe/info

---
+++ packages/mirror-media-next/pages/subscribe/info.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+import { GCP_PROJECT_ID } from '../../config/index.mjs'
+import { setPageCache } from '../../utils/cache-setting'
+import Layout from '../../components/shared/layout'
+import { ACCESS_SUBSCRIBE_FEATURE_TOGGLE } from '../../config/index.mjs'
+
+const Page = styled.main`
+  min-height: 70vh;
+  ${({ theme }) => theme.breakpoint.xl} {
+    margin-bottom: -32px;
+  }
+`
+
+/**
+
+ * @return {JSX.Element}
+ */
+function SubscribeInfo() {
+  return <Page>subscribe info page</Page>
+}
+
+export default SubscribeInfo
+
+/**
+ * @type {import('next').GetServerSideProps}
+ */
+export async function getServerSideProps({ req, res }) {
+  setPageCache(res, { cachePolicy: 'no-store' }, req.url)
+
+  const traceHeader = req.headers?.['x-cloud-trace-context']
+  let globalLogFields = {}
+  if (traceHeader && !Array.isArray(traceHeader)) {
+    const [trace] = traceHeader.split('/')
+    globalLogFields[
+      'logging.googleapis.com/trace'
+    ] = `projects/${GCP_PROJECT_ID}/traces/${trace}`
+  }
+
+  if (ACCESS_SUBSCRIBE_FEATURE_TOGGLE !== 'on') {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  } else {
+    return {
+      notFound: true,
+    }
+  }
+}
