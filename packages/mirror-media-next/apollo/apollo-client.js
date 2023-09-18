ef032fb1c1d6ed4025a26303b80bee79227b5c1c
DianYangFu <dianyangfu@gmail.com>
2023-09-18 17:19:50 +0800

    feat(mm-next): add url of preview server

--- packages/mirror-media-next/apollo/apollo-client.js
+++ packages/mirror-media-next/apollo/apollo-client.js
@@ -1,15 +1,18 @@
 import ApolloLinkTimeout from 'apollo-link-timeout'
 import { HttpLink } from '@apollo/client'
-
+import { IS_PREVIEW_MODE } from '../config/index.mjs'
 import { ApolloClient, InMemoryCache, concat } from '@apollo/client'
 
 import {
   API_TIMEOUT_GRAPHQL,
   WEEKLY_API_SERVER_ORIGIN,
+  PREVIEW_SERVER_ORIGIN,
 } from '../config/index.mjs'
 
 const httpLink = new HttpLink({
-  uri: `https://${WEEKLY_API_SERVER_ORIGIN}/content/graphql`,
+  uri: IS_PREVIEW_MODE
+    ? `https://${PREVIEW_SERVER_ORIGIN}/api/graphql`
+    : `https://${WEEKLY_API_SERVER_ORIGIN}/content/graphql`,
 })
 
 const timeoutLink = new ApolloLinkTimeout(API_TIMEOUT_GRAPHQL)
