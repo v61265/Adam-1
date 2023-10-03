1ab7664a9d44576cea67ff225f97dfc7f992b07b
hcchien <hcchien@gmail.com>
2023-10-03 21:08:13 +0800

    fix: allow the robots in prod

--- packages/mirror-media-next/pages/api/robots.js
+++ packages/mirror-media-next/pages/api/robots.js
@@ -2,15 +2,15 @@
 //Temporarily disallowed in all environment
 export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/plain')
-  res.write(`User-agent: * 
-  Disallow: /`)
+  //res.write(`User-agent: * 
+  //Disallow: /`)
 
-  //   if (ENV === 'prod') {
-  //     res.write(`User-agent: *
-  // Allow: / `)
-  //   } else {
-  //     res.write(`User-agent: *
-  // Disallow: /`)
-  //   }
+  if (ENV === 'prod') {
+     res.write(`User-agent: *
+     Allow: / `)
+  } else {
+     res.write(`User-agent: *
+     Disallow: /`)
+  }
   res.end()
 }
