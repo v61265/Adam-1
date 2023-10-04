7ccfae8dfb58e26a26d4c7d1c0ea859d710e2def
DianYangFu <dianyangfu@gmail.com>
2023-10-04 15:07:42 +0800

    refactor(mm-next): solve linter error

--- packages/mirror-media-next/pages/api/robots.js
+++ packages/mirror-media-next/pages/api/robots.js
@@ -1,15 +1,13 @@
 import { ENV } from '../../config/index.mjs'
-//Temporarily disallowed in all environment
+
 export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/plain')
-  //res.write(`User-agent: * 
-  //Disallow: /`)
 
   if (ENV === 'prod') {
-     res.write(`User-agent: *
+    res.write(`User-agent: *
      Allow: / `)
   } else {
-     res.write(`User-agent: *
+    res.write(`User-agent: *
      Disallow: /`)
   }
   res.end()
