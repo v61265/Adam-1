8164511e6dd5cb2b3e5b4cc8b6b912a5da34a15d
DianYangFu <dianyangfu@gmail.com>
2023-10-04 14:57:11 +0800

    fix(mm-next/layout): wrong property on `custom-head`

--- packages/mirror-media-next/components/shared/custom-head.js
+++ packages/mirror-media-next/components/shared/custom-head.js
@@ -117,7 +117,7 @@ export default function CustomHead(props) {
     image: {
       width: '1200',
       height: '630',
-      type: 'images-next/png',
+      type: 'image/png',
       url:
         props.imageUrl ?? `https://${SITE_URL}/images-next/default-og-img.png`,
     },
