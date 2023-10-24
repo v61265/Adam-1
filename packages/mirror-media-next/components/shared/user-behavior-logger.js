f894073332d71e4cf93d73250ed658c7a9516762
DianYangFu <dianyangfu@gmail.com>
2023-10-24 17:59:23 +0800

    feat(mm-next): generate user behavior log information

--- packages/mirror-media-next/components/shared/user-behavior-logger.js
+++ packages/mirror-media-next/components/shared/user-behavior-logger.js
@@ -1,131 +1,106 @@
-import { useEffect } from 'react'
-import { usePathname } from 'next/navigation'
+import { useEffect, useMemo } from 'react'
 import { useRouter } from 'next/router'
 import debounce from 'debounce'
 import { sendLog } from '../../utils/log/send-log'
 import { useMembership } from '../../context/membership'
-const MOCK_QUERY_PAGE_VIEW = {
-  browser: { name: 'Chrome', version: '118.0.0.0' },
-  'is-in-app-browser': false,
-  'user-agent':
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
-  'client-os': { name: 'macOS', version: '10.15.7' },
-  'curr-url': 'http://localhost:3000/',
-  datetime: '2023.10.19 17:11:06',
-  referrer: '',
-  'target-text': '',
-  'target-window-size': { width: 1905, height: 938 },
-  'client-id': 'mock-client-id',
-  'current-runtime-start': '2023.10.19 17:11:06',
-  'session-id': 'mock-session-id',
-  category: 'whole-site',
-  description: '',
-  'event-type': 'pageview',
-  'page-type': 'index',
-  'member-info-firebase': { userSignInInfo: {}, user: {} },
-  'member-info-israfel': { basicInfo: {} },
-}
 
-const MOCK_QUERY_EXIT = {
-  browser: { name: 'Chrome', version: '118.0.0.0' },
-  'is-in-app-browser': false,
-  'user-agent':
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
-  'client-os': { name: 'macOS', version: '10.15.7' },
-  'curr-url': 'http://localhost:3000/',
-  datetime: '2023.10.19 17:11:06',
-  referrer: '',
-  'target-text': '',
-  'target-window-size': { width: 1905, height: 938 },
-  'client-id': 'mock-client-id',
-  'current-runtime-start': '2023.10.19 17:11:06',
-  'session-id': 'mock-session-id',
-  category: 'whole-site',
-  description: '',
-  'event-type': 'exit',
-  'page-type': 'index',
-  'member-info-firebase': { userSignInInfo: {}, user: {} },
-  'member-info-israfel': { basicInfo: {} },
-}
-const MOCK_QUERY_SCROLL = {
-  browser: { name: 'Chrome', version: '118.0.0.0' },
-  'is-in-app-browser': false,
-  'user-agent':
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
-  'client-os': { name: 'macOS', version: '10.15.7' },
-  'curr-url': 'http://localhost:3000/',
-  datetime: '2023.10.19 17:11:06',
-  referrer: '',
-  'target-text': '',
-  'target-window-size': { width: 1905, height: 938 },
-  'client-id': 'mock-client-id',
-  'current-runtime-start': '2023.10.19 17:11:06',
-  'session-id': 'mock-session-id',
-  category: 'whole-site',
-  description: '',
-  'event-type': 'scroll',
-  'page-type': 'index',
-  'member-info-firebase': { userSignInInfo: {}, user: {} },
-  'member-info-israfel': { basicInfo: {} },
-}
+import { generateUserBehaviorLogInfo } from '../../utils/log/user-behavior-log'
 
-export default function UserBehaviorLogger() {
+export default function UserBehaviorLogger({ isMemberArticle = false }) {
   const router = useRouter()
-  const pathname = usePathname()
-  const { isLogInProcessFinished } = useMembership()
+  const { pathname } = router
+
+  const {
+    isLogInProcessFinished,
+    memberInfo: { memberType },
+    userEmail,
+    firebaseId,
+  } = useMembership()
+
+  const userBehaviorLogInfoPayload = useMemo(() => {
+    return {
+      memberType,
+      userEmail,
+      firebaseId,
+      isMemberArticle,
+    }
+  }, [memberType, userEmail, firebaseId, isMemberArticle])
 
   //pageview event
   useEffect(() => {
-    console.log(
-      'route change with dependency',
+    if (!isLogInProcessFinished) {
+      return
+    }
+
+    const info = generateUserBehaviorLogInfo(
+      'pageview',
       pathname,
-      isLogInProcessFinished
+      userBehaviorLogInfoPayload
     )
-    sendLog(MOCK_QUERY_PAGE_VIEW)
-  }, [pathname, isLogInProcessFinished])
+    sendLog(info)
+  }, [pathname, isLogInProcessFinished, userBehaviorLogInfoPayload])
 
   //exit event
   //TODO: prevent beforeunload and beforeHistoryChange trigger at the same time.
   useEffect(() => {
     let ignore = false
-    const beforeHistoryChangeEvent = () => {
-      console.log('beforeHistoryChangeEvent')
-      sendLog(MOCK_QUERY_EXIT)
-    }
-    const beforeUnloadEvent = () => {
-      if (!ignore) {
-        console.log('beforeUnloadEvent')
-        sendLog(MOCK_QUERY_EXIT)
-      }
-    }
+    let hasBeforeUnloadEventTriggered = false
 
-    router.events.on('beforeHistoryChange', beforeHistoryChangeEvent)
+    if (!isLogInProcessFinished) {
+      return
+    }
+    const info = generateUserBehaviorLogInfo(
+      'exit',
+      pathname,
+      userBehaviorLogInfoPayload
+    )
+
+    const beforeUnloadEvent = () => {
+      if (ignore) {
+        return
+      }
+      hasBeforeUnloadEventTriggered = true
+      sendLog(info)
+    }
+    const beforeHistoryChangeEvent = () => {
+      if (ignore || hasBeforeUnloadEventTriggered) {
+        return
+      }
+      sendLog(info)
+    }
     window.addEventListener('beforeunload', beforeUnloadEvent)
+    router.events.on('beforeHistoryChange', beforeHistoryChangeEvent)
     return () => {
       ignore = true
       window.removeEventListener('beforeunload', beforeUnloadEvent)
       router.events.off('beforeHistoryChange', beforeHistoryChangeEvent)
     }
-  }, [router])
+  }, [router, pathname, isLogInProcessFinished, userBehaviorLogInfoPayload])
   //scroll event
   useEffect(() => {
     let ignore = false
 
-    function detectIsScrollToBottom() {
+    if (!isLogInProcessFinished) {
+      return
+    }
+    function detectIsScrollTo80Percent() {
       const totalPageHeight = document.body.scrollHeight
       const scrollPoint = window.scrollY + window.innerHeight
-      return scrollPoint >= totalPageHeight
+      return scrollPoint >= totalPageHeight * 0.8
     }
+
     const scrollToBottomEvent = () => {
       if (ignore) {
         return
       }
-      const isScrollToBottom = detectIsScrollToBottom()
+      const isScrollToBottom = detectIsScrollTo80Percent()
       if (isScrollToBottom) {
-        console.log('isScrollToBottom')
-        sendLog(MOCK_QUERY_SCROLL)
-      } else {
-        console.log('is not ScrollToBottom')
+        const info = generateUserBehaviorLogInfo(
+          'scroll',
+          pathname,
+          userBehaviorLogInfoPayload
+        )
+        sendLog(info)
       }
     }
     window.addEventListener('scroll', debounce(scrollToBottomEvent, 500))
@@ -133,6 +108,31 @@ export default function UserBehaviorLogger() {
       ignore = true
       window.removeEventListener('scroll', debounce(scrollToBottomEvent, 500))
     }
-  }, [])
+  }, [pathname, isLogInProcessFinished, userBehaviorLogInfoPayload])
+
+  //click event
+  useEffect(() => {
+    let ignore = false
+    if (!isLogInProcessFinished) {
+      return
+    }
+    const clickEvent = () => {
+      if (ignore) {
+        return
+      }
+      const info = generateUserBehaviorLogInfo(
+        'click',
+        pathname,
+        userBehaviorLogInfoPayload
+      )
+      sendLog(info)
+    }
+    window.addEventListener('click', clickEvent)
+    return () => {
+      ignore = true
+      window.removeEventListener('click', clickEvent)
+    }
+  }, [isLogInProcessFinished, pathname, userBehaviorLogInfoPayload])
+
   return null
 }
--- packages/mirror-media-next/utils/log/send-log.js
+++ packages/mirror-media-next/utils/log/send-log.js
@@ -1,5 +1,5 @@
 const sendLog = (log, target = '/api/tracking') => {
-  const blob = new Blob([JSON.stringify({ clientInfo: log })], {
+  const blob = new Blob([JSON.stringify(log)], {
     type: 'application/json; charset=UTF-8',
   })
   navigator.sendBeacon(target, blob)
