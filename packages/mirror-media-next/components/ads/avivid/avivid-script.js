2d833af9d1886faf96206f701f6805475f1945ff
DianYangFu <dianyangfu@gmail.com>
2023-11-06 16:50:59 +0800

    feat(mm-next): only add Avivid script when viewport is desktop

--- packages/mirror-media-next/components/ads/avivid/avivid-script.js
+++ packages/mirror-media-next/components/ads/avivid/avivid-script.js
@@ -1,6 +1,14 @@
+import useWindowDimensions from '../../../hooks/use-window-dimensions'
 import Script from 'next/script'
+import { mediaSize } from '../../../styles/media'
 
 export default function AvividScript() {
+  const { width } = useWindowDimensions()
+  const isDesktopWidth = width >= mediaSize.xl
+
+  if (!isDesktopWidth) {
+    return <></>
+  }
   return (
     <Script
       strategy="lazyOnload"
