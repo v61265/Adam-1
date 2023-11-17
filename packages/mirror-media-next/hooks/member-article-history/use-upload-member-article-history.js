fdc216771a33527dad4446b42f82f1e1932135b4
DianYangFu <dianyangfu@gmail.com>
2023-11-17 15:22:15 +0800

    feat(mm-next/hooks): create custom hooks to save/upload article history

---
+++ packages/mirror-media-next/hooks/member-article-history/use-upload-member-article-history.js
@@ -0,0 +1,9 @@
+/**
+ * Work in Progress.
+ * Should implement after feature of member subscription is developed and released in Adam/mirror-media-next.
+ * Currently our feature of member subscription is executing at mirror-media-nuxt.
+ * @see https://github.com/mirror-media/mirror-media-nuxt/blob/dev/mixins/upload-member-article-history.js
+ */
+export default function useUploadMemberArticleHistory() {
+  return null
+}
