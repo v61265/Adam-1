57e9dfc1f5bd134c939cad1a0ee3f8a9e8e28cae
bibibobobi <smilingcat33@gmail.com>
2023-12-01 11:38:44 +0800

    feat(mm-next/podcast): simulate a load mor functionality

--- packages/mirror-media-next/components/podcast/podcast-list.js
+++ packages/mirror-media-next/components/podcast/podcast-list.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import PodcastCard from './podcast-card'
 
@@ -17,13 +18,61 @@ const CardsWrapper = styled.ul`
   }
 `
 
+const LoadMoreAnchor = styled.div`
+  width: 10px;
+  height: 10px;
+  background: red;
+`
+
+const LoadingSpinner = styled.img`
+  margin: auto;
+`
+
 export default function PodcastList({
   selectedPodcasts,
   allPodcasts,
   selectedAuthor,
 }) {
+  const [visiblePodcasts, setVisiblePodcasts] = useState(12)
+  const [isLoading, setIsLoading] = useState(false)
+  const loadMoreAnchorRef = useRef(null)
+
   let podcastsToDisplay =
     selectedPodcasts.length > 0 ? selectedPodcasts : allPodcasts
+
+  const loadMore = () => {
+    setIsLoading(true)
+    // Simulating a time delay of 0.8 second (800ms)
+    setTimeout(() => {
+      setVisiblePodcasts((prevVisible) => prevVisible + 12)
+      setIsLoading(false) // Set loading state to false after data is loaded
+    }, 800)
+  }
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            loadMore()
+          }
+        })
+      },
+      { threshold: 0.5 }
+    )
+
+    const anchorRefCurrent = loadMoreAnchorRef.current
+
+    if (anchorRefCurrent) {
+      observer.observe(anchorRefCurrent)
+    }
+
+    return () => {
+      if (anchorRefCurrent) {
+        observer.unobserve(anchorRefCurrent)
+      }
+    }
+  }, [])
 
   // Check if there are no podcasts for the selected author
   const noPodcastsForSelectedAuthor =
@@ -36,11 +85,18 @@ export default function PodcastList({
       {noPodcastsForSelectedAuthor ? (
         <p>There are no podcasts for the selected author, {selectedAuthor}.</p>
       ) : podcastsToDisplay.length > 0 ? (
-        <CardsWrapper>
-          {podcastsToDisplay.map((podcast) => (
-            <PodcastCard key={podcast.title} podcast={podcast} />
-          ))}
-        </CardsWrapper>
+        <>
+          <CardsWrapper>
+            {podcastsToDisplay.slice(0, visiblePodcasts).map((podcast) => (
+              <PodcastCard key={podcast.title} podcast={podcast} />
+            ))}
+          </CardsWrapper>
+          <LoadMoreAnchor ref={loadMoreAnchorRef} />
+          {/* Display the loading spinner when isLoading is true */}
+          {isLoading && (
+            <LoadingSpinner src="/images-next/loading.gif" alt="Loading" />
+          )}
+        </>
       ) : (
         <p>There are no podcasts available.</p>
       )}
