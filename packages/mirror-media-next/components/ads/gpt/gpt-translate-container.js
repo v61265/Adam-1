7a5dec926833b323733a75ba7da376c538e3c918
YiHsuan <v61265@gmail.com>
2023-12-27 13:25:08 +0800

    style(mm-next): remove height limit of gpt-translate-container

--- packages/mirror-media-next/components/ads/gpt/gpt-translate-container.js
+++ packages/mirror-media-next/components/ads/gpt/gpt-translate-container.js
@@ -13,7 +13,6 @@ import styled from 'styled-components'
  */
 
 const Container = styled.div`
-  height: 100px;
   ${
     /** @param {{rwd: Rwd, shouldTranslate: boolean}} props*/
     ({ shouldTranslate, rwd }) =>
