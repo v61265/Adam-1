b92dd48d7dbc90f10e4dabfd044c3297e7dff13f
DianYangFu <dianyangfu@gmail.com>
2024-01-13 17:13:52 +0800

    fix(mm-next): adjust election-2024 iframe url to prevent CORS

--- packages/mirror-media-next/components/election-2024-homepage.js
+++ packages/mirror-media-next/components/election-2024-homepage.js
@@ -1,6 +1,5 @@
-import { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import Image from 'next/image'
+
 import { ELECTION_2024 } from '../config/index.mjs'
 const Wrapper = styled.div`
   position: relative;
@@ -14,40 +13,9 @@ const Wrapper = styled.div`
 
 //  {/* <iframe src="https://dev.mirrormedia.mg/projects/dev-election2024-homepage-0110-7/index.html"></iframe> */}
 export default function Election2024Homepage({ className = '' }) {
-  const [shouldShowIframe, setShouldShowIframe] = useState(false)
-  useEffect(() => {
-    const currentTimeStamp = new Date().getTime()
-    const targetTimestamp = new Date('2024-01-13T08:00:00Z').getTime()
-    if (currentTimeStamp >= targetTimestamp) {
-      setShouldShowIframe(true)
-    }
-  }, [])
-
   return (
-    <>
-      <Wrapper className={className}>
-        {shouldShowIframe ? (
-          <iframe src={ELECTION_2024.url}></iframe>
-        ) : (
-          <Image
-            style={{ margin: '0 auto' }}
-            height={250}
-            width={970}
-            src="/images-next/election2024_weekly_970x250.jpg"
-            alt="2024年總統大選將於2024/1/13 16:00開始開票"
-            priority={true}
-          ></Image>
-        )}
-      </Wrapper>
-      {ELECTION_2024.shouldShowToggleButton && (
-        <button
-          onClick={() => {
-            setShouldShowIframe((pre) => !pre)
-          }}
-        >
-          測試切換
-        </button>
-      )}
-    </>
+    <Wrapper className={className}>
+      <iframe src={ELECTION_2024.url}></iframe>
+    </Wrapper>
   )
 }
