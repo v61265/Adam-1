c4cde662a6936516a6202be3780a6f632179a88b
caesar <caesar.lee@mirrormedia.mg>
2024-01-31 19:42:51 +0800

    refactor(mm): fix syntax error

--- packages/mirror-media-next/utils/amp-html.js
+++ packages/mirror-media-next/utils/amp-html.js
@@ -67,7 +67,7 @@ export function transformHtmlIntoAmpHtml(html, currentPageUrl) {
 
           replaceEle = ampYoutube
           DomUtils.replaceElement(ele, replaceEle)
-          break
+          continue
         }
         // if not youtube iframe, let the following logic to handle
       }
