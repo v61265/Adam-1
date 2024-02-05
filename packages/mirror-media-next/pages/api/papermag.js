0ac59a3d9ff54a74d4aa656de24ec5e1d20faf01
Xuan <as840922qq@gmail.com>
2024-02-05 11:54:05 +0800

    fix(papermag): update api to create order in israfel

--- packages/mirror-media-next/pages/api/papermag.js
+++ packages/mirror-media-next/pages/api/papermag.js
@@ -1,41 +1,71 @@
+import axios from 'axios'
 import NewebPay from '@mirrormedia/newebpay-node'
 import {
   NEWEBPAY_PAPERMAG_KEY,
   NEWEBPAY_PAPERMAG_IV,
+  ISRAFEL_ORIGIN,
+  SITE_URL,
+  ENV,
 } from '../../config/index.mjs'
 
-function getRandomMerchantOrderNo() {
-  const min = 1000000000
-  const max = 9999999999
-  const randomTenDigitNumber = Math.floor(Math.random() * (max - min + 1)) + min
-  return 'P' + randomTenDigitNumber
+const apiUrl = `${ISRAFEL_ORIGIN}/api/graphql`
+
+// TODO: Add JSDocs
+async function fireGqlRequest(query, variables, apiUrl) {
+  const { data: result } = await axios({
+    url: apiUrl,
+    method: 'post',
+    data: {
+      query,
+      variables,
+    },
+    headers: {
+      'content-type': 'application/json',
+      'Cache-Control': 'no-cache',
+    },
+  })
+  if (result.errors) {
+    throw new Error(result.errors)
+  }
+  return result
 }
 
-async function getPaymentDataOfPapermagSubscription(gateWayPayload) {
-  const data = {
-    MerchantID: 'MS323443601',
-    RespondType: 'JSON',
-    TimeStamp: '1694018103',
-    Version: '1.6',
-    MerchantOrderNo: getRandomMerchantOrderNo(),
-    Amt: 7280,
-    ItemDesc: '一年鏡週刊 52 期加掛號運費',
-    LoginType: 0,
-    Email: 'test@ww.ww',
-    TradeLimit: 900,
-    NotifyURL:
-      'https://mm-subscription-webhooks-publishers-dev-ufaummkd5q-de.a.run.app/newebpay/magazine',
-    ReturnURL: gateWayPayload.returnUrl,
+async function getPaymentDataOfMagazineOrders(gateWayPayload) {
+  const fetchPaymentDataOfPapermag = `mutation fetchPaymentDataOfPapermag(
+    $data: createNewebpayTradeInfoForMagazineOrderInput!
+  ) {
+    createNewebpayTradeInfoForMagazineOrder(data: $data) {
+      MerchantID
+      RespondType
+      TimeStamp
+      Version
+      MerchantOrderNo
+      Amt
+      ItemDesc
+      LoginType
+      Email
+      TradeLimit
+      NotifyURL
+    }
   }
+  `
+  const { data } = await fireGqlRequest(
+    fetchPaymentDataOfPapermag,
+    gateWayPayload,
+    apiUrl
+  )
+  data.createNewebpayTradeInfoForMagazineOrder.ReturnURL =
+    ENV === 'local'
+      ? `http://localhost:3000/papermag/return`
+      : `https://${SITE_URL}/papermag/return`
+
   return data
 }
 
 export default async function EncryptInfo(req, res) {
   const tradeInfo = req.body
   try {
-    const infoForNewebpay = await getPaymentDataOfPapermagSubscription(
-      tradeInfo.data
-    )
+    const infoForNewebpay = await getPaymentDataOfMagazineOrders(tradeInfo.data)
 
     const newebpay = new NewebPay(NEWEBPAY_PAPERMAG_KEY, NEWEBPAY_PAPERMAG_IV)
     const encryptPostData = await newebpay.getEncryptedFormPostData(
