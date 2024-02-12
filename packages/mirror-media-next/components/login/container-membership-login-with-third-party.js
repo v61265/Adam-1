8cd527e14390bffb8a8e46cda82e0283f3ca3fdf
DianYangFu <dianyangfu@gmail.com>
2024-02-12 21:19:05 +0800

    feat(mm-next): add provider in third party login

--- packages/mirror-media-next/components/login/container-membership-login-with-third-party.js
+++ packages/mirror-media-next/components/login/container-membership-login-with-third-party.js
@@ -1,3 +1,11 @@
+import { useState, useMemo } from 'react'
+import { auth } from '../../firebase'
+import {
+  OAuthProvider,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  signInWithRedirect,
+} from 'firebase/auth'
 /** @typedef {'Google' | 'Facebook' | 'Apple'} ThirdPartyName */
 
 /**
@@ -8,5 +16,35 @@
 export default function ContainerMembershipLoginWithThirdParty({
   thirdPartyName,
 }) {
-  return <button>第三方{thirdPartyName}登入</button>
+  const provider = useMemo(() => {
+    let provider = null
+    switch (thirdPartyName) {
+      case 'Google':
+        provider = new GoogleAuthProvider()
+        break
+      case 'Facebook':
+        provider = new FacebookAuthProvider()
+        break
+      case 'Apple':
+        provider = new OAuthProvider('apple.com')
+        break
+    }
+    provider.addScope('email')
+    return provider
+  }, [thirdPartyName])
+
+  const [isLoading, setIsLoading] = useState(false)
+  const handleThirdPartyFirebaseLogin = async () => {
+    setIsLoading(true)
+    await signInWithRedirect(auth, provider)
+  }
+  return (
+    <button onClick={handleThirdPartyFirebaseLogin}>
+      {isLoading ? (
+        <span>載入中...</span>
+      ) : (
+        <span>第三方{thirdPartyName}登入</span>
+      )}
+    </button>
+  )
 }
