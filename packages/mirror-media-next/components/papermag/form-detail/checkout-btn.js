3ce565aac047c042f2f7405709e2a58df167cc4d
Xuan <as840922qq@gmail.com>
2024-02-16 12:36:42 +0800

    feat(papermag): temporarily remove required for test

--- packages/mirror-media-next/components/papermag/form-detail/checkout-btn.js
+++ packages/mirror-media-next/components/papermag/form-detail/checkout-btn.js
@@ -43,7 +43,8 @@ const StyledCheckoutBtn = styled.button`
 
 export default function CheckoutBtn({ isAcceptedConditions, receiptOption }) {
   const [isLoading, setIsLoading] = useState(false)
-  const isButtonDisabled = !isAcceptedConditions || receiptOption === null
+  // const isButtonDisabled = !isAcceptedConditions || receiptOption === null
+  const isButtonDisabled = false
 
   const handleCheckout = () => {
     if (!isAcceptedConditions || receiptOption === null) {
