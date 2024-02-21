24971ab96ab008940b3fcfb51929cd894c823e9c
DianYangFu <dianyangfu@gmail.com>
2024-02-21 19:07:15 +0800

    feat(mm-next): add feature for email password login

--- packages/mirror-media-next/components/login/container-login-form-login-with-password.js
+++ packages/mirror-media-next/components/login/container-login-form-login-with-password.js
@@ -1,3 +1,98 @@
+import { useState } from 'react'
+import UiMembershipInput from './ui/input/ui-membership-input'
+import { useAppSelector, useAppDispatch } from '../../hooks/useRedux'
+import {
+  setPassword,
+  loginPassword,
+  changeLoginFormMode,
+  clearPassword,
+  loginEmail,
+  changeState,
+} from '../../slice/login-slice'
+import { auth } from '../../firebase'
+import {
+  getAccessToken,
+  errorHandler,
+  loginPageOnAuthStateChangeAction,
+} from '../../utils/membership'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+
+import UiMembershipButton from './ui/button/ui-membership-button'
+import { FirebaseError } from 'firebase/app'
 export default function ContainerLoginFormLoginWithPassword() {
-  return <div>ContainerLoginFormLoginWithPassword</div>
+  const dispatch = useAppDispatch()
+  const password = useAppSelector(loginPassword)
+  const email = useAppSelector(loginEmail)
+  const [shouldShowErrorHint, setShouldShowErrorHint] = useState(false)
+  // const shouldShowErrorHint = useAppSelector(loginShouldShowErrorHint)
+  const handlePasswordOnChange = (e) => {
+    dispatch(setPassword(e.target.value))
+  }
+  const [shouldRevealPassword, setShouldRevealPassword] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const handleRevealPasswordButtonClick = () => {
+    setShouldRevealPassword((pre) => !pre)
+  }
+  //TODO: better name
+  const handleGoBack = () => {
+    dispatch(changeLoginFormMode('initial'))
+    dispatch(clearPassword())
+    setShouldShowErrorHint(false)
+  }
+  const handleSubmit = async () => {
+    setIsLoading(true)
+    try {
+      const { user } = await signInWithEmailAndPassword(auth, email, password)
+      if (!user) {
+        throw new Error('Unable to get Firebase User')
+      }
+      const idToken = await user.getIdToken()
+      if (!idToken) {
+        throw new Error('Unexpected Error when getting firebase Id token.')
+      }
+      const accessToken = await getAccessToken(idToken)
+      const result = await loginPageOnAuthStateChangeAction(
+        user,
+        'login',
+        false,
+        accessToken
+      )
+      dispatch(changeState(result))
+      setIsLoading(false)
+    } catch (e) {
+      setIsLoading(false)
+
+      if (e instanceof FirebaseError && e.code === 'auth/wrong-password') {
+        setShouldShowErrorHint(true)
+      } else {
+        errorHandler(e)
+        dispatch(changeState('loginError'))
+      }
+    }
+  }
+  return (
+    <div>
+      登入模式
+      <br></br>
+      <UiMembershipInput
+        placeholder="密碼大於六位數"
+        value={password}
+        type={shouldRevealPassword ? 'text' : 'password'}
+        onChange={handlePasswordOnChange}
+      ></UiMembershipInput>
+      <br></br>
+      <button onClick={handleRevealPasswordButtonClick}>
+        {shouldRevealPassword ? '隱藏密碼' : '顯示密碼'}
+      </button>
+      <br></br>
+      <UiMembershipButton buttonType="primary" handleOnClick={handleSubmit}>
+        <p>{isLoading ? '載入中' : '登入'}</p>
+      </UiMembershipButton>
+      <br></br>
+      <UiMembershipButton buttonType="secondary" handleOnClick={handleGoBack}>
+        <p>回上一步</p>
+      </UiMembershipButton>
+      {shouldShowErrorHint && <p>密碼錯誤，請重新嘗試</p>}
+    </div>
+  )
 }
