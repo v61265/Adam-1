9e706007a74cad485dc58ddd107c3ae6fe8092dc
DianYangFu <dianyangfu@gmail.com>
2024-02-21 19:05:47 +0800

    feat(mm-next): add feature for checking email is register or login with third party way

--- packages/mirror-media-next/components/login/container-login-form-initial.js
+++ packages/mirror-media-next/components/login/container-login-form-initial.js
@@ -1,6 +1,18 @@
+import { fetchSignInMethodsForEmail } from 'firebase/auth'
 import ContainerMembershipLoginWithThirdParty from './container-membership-login-with-third-party'
 import UiMembershipInputEmailInvalidation from './ui-membership-input-email-invalidation'
 import UiMembershipButton from './ui/button/ui-membership-button'
+import { auth } from '../../firebase'
+import { useState } from 'react'
+import { useAppSelector, useAppDispatch } from '../../hooks/useRedux'
+import {
+  loginEmail,
+  changeLoginFormMode,
+  changePrevAuthMethod,
+  changeShouldShowHint,
+  loginPrevAuthMethod,
+  loginShouldShowHint,
+} from '../../slice/login-slice'
 /**
  * @typedef {import('./container-membership-login-with-third-party').ThirdPartyName} ThirdPartyName
  */
@@ -14,6 +26,68 @@ const THIRD_PARTY_LIST = [
   { name: 'Apple' },
 ]
 export default function ContainerLoginFormInitial() {
+  const [isLoading, setIsLoading] = useState(false)
+  const dispatch = useAppDispatch()
+  const email = useAppSelector(loginEmail)
+  const prevAuthMethod = useAppSelector(loginPrevAuthMethod)
+  const shouldShowHint = useAppSelector(loginShouldShowHint)
+  const hint = `由於您曾以 ${prevAuthMethod} 帳號登入，請點擊上方「使用${prevAuthMethod}帳號繼續」重試。`
+  const handleOnClick = async () => {
+    setIsLoading(true)
+    try {
+      const responseArray = await fetchSignInMethodsForEmail(auth, email)
+
+      if (!responseArray.length) {
+        dispatch(changeLoginFormMode('register'))
+        return
+      }
+
+      /*
+       * Hint, If email verify is active in the future,
+       * responseArray would have multi value
+       */
+      const isEmailExistWithEmailLinkSignInMethod =
+        responseArray &&
+        responseArray.find((signInMethod) => signInMethod === 'emailLink')
+
+      const isEmailExistWithEmailPasswordSignInMethod =
+        responseArray &&
+        responseArray.find((signInMethod) => signInMethod === 'password')
+
+      const isEmailHasBeenUsedByGoogleAuth =
+        responseArray &&
+        responseArray.find((signInMethod) => signInMethod === 'google.com')
+
+      const isEmailHasBeenUsedByFacebookAuth =
+        responseArray &&
+        responseArray.find((signInMethod) => signInMethod === 'facebook.com')
+
+      const isEmailHasBeenUsedByAppleAuth =
+        responseArray &&
+        responseArray.find((signInMethod) => signInMethod === 'apple.com')
+      if (isEmailExistWithEmailLinkSignInMethod) {
+        dispatch(changeLoginFormMode('recoverPassword'))
+      } else if (isEmailExistWithEmailPasswordSignInMethod) {
+        dispatch(changeLoginFormMode('login'))
+        dispatch(changePrevAuthMethod('email'))
+        dispatch(changeShouldShowHint(true))
+      } else if (isEmailHasBeenUsedByGoogleAuth) {
+        dispatch(changePrevAuthMethod('Google'))
+        dispatch(changeShouldShowHint(true))
+      } else if (isEmailHasBeenUsedByFacebookAuth) {
+        dispatch(changePrevAuthMethod('Facebook'))
+        dispatch(changeShouldShowHint(true))
+      } else if (isEmailHasBeenUsedByAppleAuth) {
+        dispatch(changePrevAuthMethod('Apple'))
+        dispatch(changeShouldShowHint(true))
+      }
+
+      setIsLoading(false)
+    } catch (e) {
+      console.log(e)
+      setIsLoading(false)
+    }
+  }
   return (
     <div>
       ContainerLoginFormInitial
@@ -24,9 +98,12 @@ export default function ContainerLoginFormInitial() {
           thirdPartyName={item.name}
         />
       ))}
+      {shouldShowHint && <p>{hint}</p>}
       <p>或</p>
       <UiMembershipInputEmailInvalidation></UiMembershipInputEmailInvalidation>
-      <UiMembershipButton buttonType={'primary'}>下一步</UiMembershipButton>
+      <UiMembershipButton buttonType={'primary'} handleOnClick={handleOnClick}>
+        {isLoading ? '載入中...' : '下一步'}
+      </UiMembershipButton>
     </div>
   )
 }
