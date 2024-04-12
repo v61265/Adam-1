0fea56130c994dfb0ba016e2e763e30ab3398021
Tsuki Akiba <erase2004@gmail.com>
2024-04-12 17:41:39 +0800

    fix(mm-next): query params should be kept while changing from index page to login page

--- packages/mirror-media-next/components/header/normal/member-login-button.js
+++ packages/mirror-media-next/components/header/normal/member-login-button.js
@@ -96,6 +96,7 @@ export default function MemberLoginButton() {
       return {
         pathname: '/login',
         query: {
+          ...router.query,
           destination: '/',
         },
       }
