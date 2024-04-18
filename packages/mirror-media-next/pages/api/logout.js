51bd6238e23d5872af34781802a71133ece20ff3
Tsuki Akiba <erase2004@gmail.com>
2024-04-18 11:18:35 +0800

    feat(mm-next): add `/api/logout`

---
+++ packages/mirror-media-next/pages/api/logout.js
@@ -0,0 +1,62 @@
+import Cors from 'cors'
+import { SESSION_COOKIE_NAME } from '../../config/index.mjs'
+import { runMiddleware } from '../../utils/api-route'
+import cookie from 'cookie'
+import { getAdminAuth } from '../../firebase/admin'
+
+const cors = Cors({
+  methods: ['POST'],
+})
+
+/**
+ * remove session cookie
+ *
+ * @param {import("next").NextApiRequest} req
+ * @param {import("next").NextApiResponse} res
+ */
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res
+      .status(405)
+      .setHeader('Allow', 'HEAD, POST')
+      .json({ error: 'Method Not Allowed' })
+  }
+
+  try {
+    await runMiddleware(req, res, cors)
+
+    const sessionCookie = req.cookies[SESSION_COOKIE_NAME]
+    if (!sessionCookie) throw new Error('seesion cookie in required')
+
+    const decodedClaims = await getAdminAuth().verifySessionCookie(
+      sessionCookie
+    )
+    await getAdminAuth().revokeRefreshTokens(decodedClaims.sub)
+
+    return res
+      .status(200)
+      .setHeader(
+        'Set-Cookie',
+        cookie.serialize(SESSION_COOKIE_NAME, '', {
+          expires: new Date(0), // set time in the past to remove cookie
+          httpOnly: true,
+          secure: false,
+          path: '/',
+        })
+      )
+      .json({
+        status: 'success',
+      })
+  } catch (err) {
+    console.error(
+      JSON.stringify({
+        severity: 'ERROR',
+        message: err.message,
+      })
+    )
+
+    return res.status(500).json({
+      error: err.message,
+    })
+  }
+}
