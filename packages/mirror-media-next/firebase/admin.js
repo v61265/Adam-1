b94d8376999a447ec2c4f4fb14236f7c14fbe0f8
Tsuki Akiba <erase2004@gmail.com>
2024-04-23 19:20:12 +0800

    fix(mm-next): firebase-admin related build error

--- packages/mirror-media-next/firebase/admin.js
+++ packages/mirror-media-next/firebase/admin.js
@@ -2,19 +2,18 @@ import { FIREBASE_ADMIN_CREDENTIAL } from '../config/index.mjs'
 import { initializeApp, getApp, cert } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 
-let app
-try {
-  app = getApp()
-} catch (e) {
-  app = initializeApp({ credential: cert(FIREBASE_ADMIN_CREDENTIAL) })
-}
-
 function getAdminApp() {
+  let app
+  try {
+    app = getApp()
+  } catch (e) {
+    app = initializeApp({ credential: cert(FIREBASE_ADMIN_CREDENTIAL) })
+  }
   return app
 }
 
 function getAdminAuth() {
-  return getAuth(app)
+  return getAuth(getAdminApp())
 }
 
 export { getAdminApp, getAdminAuth }
