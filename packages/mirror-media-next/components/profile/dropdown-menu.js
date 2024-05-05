ef112586920b5fc3af41d39923545bdbfd590587
nihclil <oxx921@gmail.com>
2024-05-05 22:06:25 +0800

    feat(mm-next): add main UI elements to profile page

---
+++ packages/mirror-media-next/components/profile/dropdown-menu.js
@@ -0,0 +1,121 @@
+import styled from 'styled-components'
+import { useState } from 'react'
+
+const DropdownMenuWrapper = styled.div`
+  position: relative;
+`
+
+const SelectBox = styled.div`
+  position: relative;
+  border: 1px solid ${(props) => (props.isOpen ? '#000000de' : '#0000004d')};
+  border-bottom: ${(props) => (props.isOpen ? 'none' : '1px solid #0000004d')};
+  border-radius: ${(props) => (props.isOpen ? '8px 8px 0 0' : '8px')};
+  color: ${(props) => (props.isSelected ? '#000000DE' : '#0000004d')};
+  cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
+  padding: 12px;
+
+  ::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    right: 14px;
+    border: solid #054f77;
+    border-width: 0 0 2px 2px;
+    padding: 4px;
+    transform: translateY(-60%) rotate(-45deg);
+  }
+
+  ::before {
+    content: '';
+    position: absolute;
+    bottom: 4px;
+    left: 12px;
+    right: 12px;
+    height: 1px;
+    background-color: ${(props) =>
+      props.isOpen ? '#0000004d' : 'transparent'};
+  }
+`
+
+const Ul = styled.ul`
+  position: absolute;
+  background-color: #fff;
+  width: 100%;
+  z-index: 100;
+  border: 1px solid #000000de;
+  border-top: none;
+  border-bottom-right-radius: 8px;
+  border-bottom-left-radius: 8px;
+  overflow: hidden;
+  max-height: 160px; // limit the visible items
+  overflow-y: auto;
+  z-index: 100;
+`
+const Li = styled.li`
+  padding: 8px 12px;
+  :hover {
+    cursor: pointer;
+    color: #fff;
+    background-color: #054f77;
+  }
+`
+/**
+ * @param {Object} props
+ * @param {object} props.options
+ * @param {string} props.keyField
+ * @param {string} props.value
+ * @param {function(string):void} props.onSelect
+ * @param {string} props.selectedOption
+ * @param {boolean} [props.disabled=false]
+ * @returns {JSX.Element}
+ */
+
+export default function DropdownMenu({
+  options,
+  keyField,
+  value,
+  onSelect,
+  selectedOption,
+  disabled = false,
+}) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleDropdown = () => {
+    if (!disabled) {
+      setIsOpen(!isOpen)
+    }
+  }
+
+  const handleClickOption = (option) => {
+    if (!disabled) {
+      onSelect(option[value])
+      setIsOpen(!isOpen)
+    }
+  }
+
+  return (
+    <DropdownMenuWrapper>
+      <SelectBox
+        isOpen={isOpen}
+        isSelected={!!selectedOption}
+        disabled={disabled}
+        onClick={toggleDropdown}
+      >
+        {selectedOption ? selectedOption : '請選擇'}
+      </SelectBox>
+
+      {isOpen && (
+        <Ul>
+          {options.map((option) => (
+            <Li
+              key={option[keyField]}
+              onClick={() => handleClickOption(option)}
+            >
+              {option[value]}
+            </Li>
+          ))}
+        </Ul>
+      )}
+    </DropdownMenuWrapper>
+  )
+}
---
+++ packages/mirror-media-next/components/profile/user-profile-form.js
@@ -0,0 +1,325 @@
+import styled from 'styled-components'
+import { useState } from 'react'
+import countryOptions from 'constants/lib/countries.json'
+import taiwanDisTrictOptions from 'constants/lib/taiwan-districts.json'
+import DropdownMenu from './dropdown-menu'
+import PrimaryButton from '../shared/buttons/primary-button'
+import DefaultButton from '../shared/buttons/default-button'
+
+const FormWrapper = styled.div`
+  ${({ theme }) => theme.breakpoint.md} {
+    width: 600px;
+    margin: auto;
+  }
+  ${({ theme }) => theme.breakpoint.xl} {
+    width: 680px;
+    margin: auto;
+  }
+`
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 36px;
+  margin-bottom: 24px;
+  ${({ theme }) => theme.breakpoint.xl} {
+    margin-bottom: 12px;
+  }
+`
+
+const Form = styled.form`
+  ${({ theme }) => theme.breakpoint.xl} {
+    border: 1px solid #0000001a;
+    border-radius: 2px;
+    padding: 40px;
+  }
+`
+
+const EmailWrapper = styled.div`
+  margin-bottom: 40px;
+
+  h2 {
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 27px;
+    margin-bottom: 8px;
+  }
+
+  p {
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 27px;
+    color: #000000de;
+  }
+`
+
+const PasswordWrapper = styled.div`
+  margin-bottom: 40px;
+  display: none;
+
+  h2 {
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 27px;
+    margin-bottom: 8px;
+  }
+
+  ${({ theme }) => theme.breakpoint.md} {
+    display: block;
+  }
+`
+const PasswordButtonWrapper = styled.div`
+  width: 104px;
+  height: 48px;
+`
+
+const FlexRowBox = styled.div`
+  ${({ theme }) => theme.breakpoint.md} {
+    display: flex;
+    flex-direction: row;
+    gap: 16px;
+  }
+`
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 40px;
+  width: 100%;
+
+  input {
+    border: 1px solid rgba(0, 0, 0, 0.3);
+    border-radius: 8px;
+    padding: 12px;
+    ::placeholder {
+      color: rgba(0, 0, 0, 0.3);
+    }
+  }
+`
+
+const FormDetails = styled.div`
+  margin-bottom: 40px;
+
+  h2 {
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 27px;
+    margin-bottom: 16px;
+  }
+
+  > div {
+    margin-bottom: 24px;
+  }
+
+  div:last-child {
+    margin-bottom: 0;
+  }
+`
+
+const ItemsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-bottom: 24px;
+
+  input {
+    border: 1px solid rgba(0, 0, 0, 0.3);
+    border-radius: 8px;
+    padding: 12px;
+    ::placeholder {
+      color: rgba(0, 0, 0, 0.3);
+    }
+  }
+
+  ${({ theme }) => theme.breakpoint.md} {
+    margin-bottom: 0;
+  }
+`
+
+const LargeLabel = styled.label`
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 27px;
+  margin-bottom: 8px;
+`
+
+const SmallLabel = styled.label`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 28px;
+  color: rgba(0, 0, 0, 0.5);
+  margin-bottom: 4px;
+`
+
+const ButtonWrapper = styled.div`
+  margin-bottom: 40px;
+
+  ${({ theme }) => theme.breakpoint.md} {
+    width: 132px;
+    height: 48px;
+    margin: auto;
+  }
+`
+
+const genderOptions = [
+  { id: 1, name: '不透露' },
+  { id: 2, name: '男' },
+  { id: 3, name: '女' },
+]
+
+const monthOptions = [
+  { id: 1, name: '1' },
+  { id: 2, name: '2' },
+  { id: 3, name: '3' },
+  { id: 4, name: '4' },
+  { id: 5, name: '5' },
+  { id: 6, name: '6' },
+  { id: 7, name: '7' },
+  { id: 8, name: '8' },
+  { id: 9, name: '9' },
+  { id: 10, name: '10' },
+  { id: 11, name: '11' },
+  { id: 12, name: '12' },
+]
+
+/**
+ * @returns {JSX.Element}
+ */
+
+export default function UserProfileForm() {
+  const [selectedGender, setSelectedGender] = useState('')
+  const [selectedMonth, setSelectedMonth] = useState('')
+  const [selectedCountry, setSelectedCountry] = useState('')
+  const [selectedCity, setSelectedCity] = useState('')
+  const [districtData, setDistrictData] = useState([])
+  const [selectedDistrict, setSelectedDistrict] = useState('')
+
+  const handleCountrySelect = (country) => {
+    setSelectedCountry(country)
+    if (country !== '台灣') {
+      setSelectedCity('')
+      setSelectedDistrict('')
+    }
+  }
+
+  const handleCitySelect = (city) => {
+    setSelectedCity(city)
+    const cityData = taiwanDisTrictOptions.find(function (el) {
+      return el.name === city
+    })
+    setDistrictData(cityData.districts)
+  }
+
+  return (
+    <FormWrapper>
+      <Title>個人資料</Title>
+      <Form>
+        <EmailWrapper>
+          <h2>Email</h2>
+          <p>test email</p>
+        </EmailWrapper>
+
+        <PasswordWrapper>
+          <h2>密碼</h2>
+          <PasswordButtonWrapper>
+            <DefaultButton>變更密碼</DefaultButton>
+          </PasswordButtonWrapper>
+        </PasswordWrapper>
+
+        <FlexRowBox>
+          <FormGroup>
+            <LargeLabel htmlFor="name">姓名</LargeLabel>
+            <input id="name" placeholder="請輸入姓名" />
+          </FormGroup>
+          <FormGroup>
+            <LargeLabel>性別</LargeLabel>
+            <DropdownMenu
+              keyField="id"
+              value="name"
+              options={genderOptions}
+              selectedOption={selectedGender}
+              onSelect={setSelectedGender}
+            />
+          </FormGroup>
+        </FlexRowBox>
+
+        <FormDetails>
+          <h2>生日</h2>
+
+          <FlexRowBox>
+            <ItemsWrapper>
+              <SmallLabel htmlFor="year">西元年</SmallLabel>
+              <input id="year" placeholder="西元年" />
+            </ItemsWrapper>
+            <ItemsWrapper>
+              <SmallLabel>月份</SmallLabel>
+              <DropdownMenu
+                options={monthOptions}
+                keyField="id"
+                value="name"
+                selectedOption={selectedMonth}
+                onSelect={setSelectedMonth}
+              />
+            </ItemsWrapper>
+            <ItemsWrapper>
+              <SmallLabel htmlFor="date">日期</SmallLabel>
+              <input id="date" placeholder="日期" />
+            </ItemsWrapper>
+          </FlexRowBox>
+        </FormDetails>
+
+        <FormGroup>
+          <LargeLabel htmlFor="phone">電話</LargeLabel>
+          <input id="phone" placeholder="電話" />
+        </FormGroup>
+        <FormDetails>
+          <h2>地址</h2>
+
+          <ItemsWrapper>
+            <SmallLabel>國家</SmallLabel>
+            <DropdownMenu
+              options={countryOptions}
+              keyField="English"
+              value="Taiwan"
+              selectedOption={selectedCountry}
+              onSelect={handleCountrySelect}
+            />
+          </ItemsWrapper>
+
+          <FlexRowBox>
+            <ItemsWrapper>
+              <SmallLabel htmlFor="city">縣市</SmallLabel>
+              <DropdownMenu
+                options={taiwanDisTrictOptions}
+                keyField="name"
+                value="name"
+                selectedOption={selectedCity}
+                onSelect={handleCitySelect}
+                disabled={selectedCountry !== '臺灣'}
+              />
+            </ItemsWrapper>
+            <ItemsWrapper>
+              <SmallLabel htmlFor="district">行政區</SmallLabel>
+              <DropdownMenu
+                options={districtData}
+                keyField="zip"
+                value="name"
+                selectedOption={selectedDistrict}
+                onSelect={setSelectedDistrict}
+                disabled={selectedCountry !== '臺灣' || selectedCity == ''}
+              />
+            </ItemsWrapper>
+          </FlexRowBox>
+
+          <ItemsWrapper>
+            <SmallLabel htmlFor="address">詳細地址</SmallLabel>
+            <input id="address" placeholder="詳細地址" />
+          </ItemsWrapper>
+        </FormDetails>
+        <ButtonWrapper>
+          <PrimaryButton isLoading={false}>儲存</PrimaryButton>
+        </ButtonWrapper>
+      </Form>
+    </FormWrapper>
+  )
+}
