4c4c88f9fb60452c03bd22f274f7a7cfabb64289
YiHsuan <v61265@gmail.com>
2024-05-14 14:02:19 +0800

    refactor(mm-next/gpt-placeholder): add console to debug

--- packages/mirror-media-next/components/ads/gpt/gpt-placeholder.js
+++ packages/mirror-media-next/components/ads/gpt/gpt-placeholder.js
@@ -25,20 +25,20 @@ const Container = styled.div`
     ({ rwd }) => rwd.mobile.width
   };
   min-height: ${({ rwd, shouldTranslate }) =>
-    shouldTranslate ? '0px' : rwd.mobile.height};
+    shouldTranslate ? rwd.mobile.height : '0px'};
   margin: ${({ rwd }) => rwd.mobile.margin};
 
   ${({ theme }) => theme.breakpoint.md} {
     min-width: ${({ rwd }) => rwd.tablet.width};
     min-height: ${({ rwd, shouldTranslate }) =>
-      shouldTranslate ? '0px' : rwd.tablet.height};
+      shouldTranslate ? rwd.tablet.height : '0px'};
     margin: ${({ rwd }) => rwd.tablet.margin};
   }
 
   ${({ theme }) => theme.breakpoint.xl} {
     min-width: ${({ rwd }) => rwd.desktop.width};
     min-height: ${({ rwd, shouldTranslate }) => {
-      return shouldTranslate ? '0px' : rwd.desktop.height
+      return shouldTranslate ? rwd.desktop.height : '0px'
     }};
     margin: ${({ rwd }) => rwd.desktop.margin};
   }
@@ -93,6 +93,11 @@ export default function GPT_Placeholder({
     // 原因可參見該描述：https://app.asana.com/0/1181156545719626/1206976463067891/f
     setIsMounted(true)
   }, [])
+
+  useEffect(() => {
+    console.log({ shouldTranslate, isMounted })
+  }, [isMounted, shouldTranslate])
+
   return (
     <Container rwd={rwd} shouldTranslate={shouldTranslate && isMounted}>
       {children}
