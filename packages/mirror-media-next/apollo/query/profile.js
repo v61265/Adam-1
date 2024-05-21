ddde2174d1f566670ff3d757c8498f80f4de51b8
nihclil <oxx921@gmail.com>
2024-05-21 11:29:54 +0800

    refactor(mm-next): remove jsdoc comments

--- packages/mirror-media-next/apollo/query/profile.js
+++ packages/mirror-media-next/apollo/query/profile.js
@@ -1,20 +1,5 @@
 import { gql } from '@apollo/client'
 
-/**
- * @typedef {Object} Member
- * @property {string} id
- * @property {string | null} firebaseId
- * @property {string | null} email
- * @property {string | null} name
- * @property {string | null} gender
- * @property {string | null} birthday
- * @property {string | null} phone
- * @property {string | null} country
- * @property {string | null} city
- * @property {string | null} district
- * @property {string | null} address
- */
-
 export const fetchMemberProfileByFirebaseId = gql`
   query fetchMemberProfile($firebaseId: String!) {
     member(where: { firebaseId: $firebaseId }) {
