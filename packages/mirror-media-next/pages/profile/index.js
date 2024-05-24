94be8e4a5e4d307c7bd6df1ef6aa234f9ea11f47
nihclil <oxx921@gmail.com>
2024-05-24 23:20:28 +0800

    refactor(mm-next):use useMembershipRequired and redirectToLoginWhileUnauthed

--- packages/mirror-media-next/pages/profile/index.js
+++ packages/mirror-media-next/pages/profile/index.js
@@ -4,13 +4,14 @@ import UserProfileForm from '../../components/profile/user-profile-form'
 import UserDeletionForm from '../../components/profile/user-deletion-form'
 import { useMembership } from '../../context/membership'
 import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
 import client from '../../apollo/apollo-client'
 import { fetchMemberProfileByFirebaseId } from '../../apollo/profile/query/fetch-member-profile'
 import { GCP_PROJECT_ID } from '../../config/index.mjs'
 import errors from '@twreporter/errors'
 import { fetchHeaderDataInDefaultPageLayout } from '../../utils/api'
 import { setPageCache } from '../../utils/cache-setting'
+import useMembershipRequired from '../../hooks/use-membership-required'
+import redirectToLoginWhileUnauthed from '../../utils/redirect-to-login-while-unauthed'
 
 const Page = styled.main`
   padding: 40px 20px;
@@ -47,19 +48,10 @@ const Title = styled.h1`
  */
 
 export default function Profile({ sectionsData = [], topicsData = [] }) {
-  const router = useRouter()
-
-  const { isLoggedIn, isLogInProcessFinished, accessToken, firebaseId } =
-    useMembership()
+  useMembershipRequired()
+  const { accessToken, firebaseId } = useMembership()
 
   const [profile, setProfile] = useState({})
-
-  useEffect(() => {
-    if (isLogInProcessFinished && !isLoggedIn) {
-      const destination = encodeURIComponent('/profile')
-      router.push(`/login?destination=${destination}`)
-    }
-  }, [isLogInProcessFinished, isLoggedIn, router])
 
   useEffect(() => {
     const fetchMemberProfile = async () => {
@@ -93,8 +85,6 @@ export default function Profile({ sectionsData = [], topicsData = [] }) {
     }
   }, [firebaseId, accessToken])
 
-  if (!isLoggedIn) return null
-
   return (
     <LayoutFull
       head={{ title: `個人資料` }}
@@ -116,47 +106,49 @@ export default function Profile({ sectionsData = [], topicsData = [] }) {
 /**
  * @type {import('next').GetServerSideProps}
  */
-export async function getServerSideProps({ req, res }) {
-  setPageCache(res, { cachePolicy: 'no-store' }, req.url)
+export const getServerSideProps = redirectToLoginWhileUnauthed()(
+  async ({ req, res }) => {
+    setPageCache(res, { cachePolicy: 'no-store' }, req.url)
 
-  const traceHeader = req.headers?.['x-cloud-trace-context']
-  let globalLogFields = {}
-  if (traceHeader && !Array.isArray(traceHeader)) {
-    const [trace] = traceHeader.split('/')
-    globalLogFields[
-      'logging.googleapis.com/trace'
-    ] = `projects/${GCP_PROJECT_ID}/traces/${trace}`
-  }
-
-  let sectionsData = []
-  let topicsData = []
-
-  try {
-    const headerData = await fetchHeaderDataInDefaultPageLayout()
-    if (Array.isArray(headerData.sectionsData)) {
-      sectionsData = headerData.sectionsData
+    const traceHeader = req.headers?.['x-cloud-trace-context']
+    let globalLogFields = {}
+    if (traceHeader && !Array.isArray(traceHeader)) {
+      const [trace] = traceHeader.split('/')
+      globalLogFields[
+        'logging.googleapis.com/trace'
+      ] = `projects/${GCP_PROJECT_ID}/traces/${trace}`
     }
-    if (Array.isArray(headerData.topicsData)) {
-      topicsData = headerData.topicsData
-    }
-  } catch (err) {
-    const annotatingAxiosError = errors.helpers.annotateAxiosError(err)
-    console.error(
-      JSON.stringify({
-        severity: 'ERROR',
-        message: errors.helpers.printAll(annotatingAxiosError, {
-          withStack: true,
-          withPayload: true,
-        }),
-        ...globalLogFields,
-      })
-    )
-  }
 
-  return {
-    props: {
-      sectionsData,
-      topicsData,
-    },
+    let sectionsData = []
+    let topicsData = []
+
+    try {
+      const headerData = await fetchHeaderDataInDefaultPageLayout()
+      if (Array.isArray(headerData.sectionsData)) {
+        sectionsData = headerData.sectionsData
+      }
+      if (Array.isArray(headerData.topicsData)) {
+        topicsData = headerData.topicsData
+      }
+    } catch (err) {
+      const annotatingAxiosError = errors.helpers.annotateAxiosError(err)
+      console.error(
+        JSON.stringify({
+          severity: 'ERROR',
+          message: errors.helpers.printAll(annotatingAxiosError, {
+            withStack: true,
+            withPayload: true,
+          }),
+          ...globalLogFields,
+        })
+      )
+    }
+
+    return {
+      props: {
+        sectionsData,
+        topicsData,
+      },
+    }
   }
-}
+)
