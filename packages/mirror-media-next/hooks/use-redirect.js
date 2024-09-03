6593b40e24182992ee296d1086948013879755cd
Tsuki Akiba <erase2004@gmail.com>
2024-09-03 19:04:34 +0800

    refactor(mm-next): replace router.replace with location.replace to prevent client-side render issue

--- packages/mirror-media-next/hooks/use-redirect.js
+++ packages/mirror-media-next/hooks/use-redirect.js
@@ -5,16 +5,30 @@ import { useCallback } from 'react'
 /**
  *
  * @param {import('next/router').NextRouter} router
- * @returns {import('next/link').LinkProps['href']}
+ * @returns {string}
  */
 const getDestination = (router) => {
   const destination = router?.query?.destination
 
   if (!destination) {
-    return {
-      pathname: defaultDestination,
-      query: router.query,
+    const url = new URL(window.location.origin)
+    url.pathname = defaultDestination
+
+    /** @type {[string, string][]} */
+    const entries = []
+    for (const [key, value] of Object.entries(router.query)) {
+      if (Array.isArray(value)) {
+        for (const v of value) {
+          entries.push([key, v])
+        }
+      } else {
+        entries.push([key, value])
+      }
     }
+
+    url.search = new URLSearchParams(entries).toString()
+
+    return url.href
   }
 
   const queryParam = Object.assign({}, router.query)
@@ -42,7 +56,9 @@ export default function useRedirect() {
     }
     const destination = getDestination(router)
 
-    router.replace(destination)
+    // TODO: there are some prviledged pages under construction,
+    // so we can't use router.replace
+    window.location.replace(destination)
   }, [router])
 
   return { redirect }
